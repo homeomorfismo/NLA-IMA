a74c5d0ffb5b7eddc68358b80fb5cdfd15ab17c3
Gabo <gpinochetsoto@gmail.com>
2021-03-29 11:40:55 -0300

    HW1

--- hw1/LowTriMatMult.m
+++ hw1/LowTriMatMult.m
@@ -4,15 +4,14 @@ function [C] = LowTriMatMult(A,B)
 if (size(A)~=size(B))
     disp('Unable to compute A*B: matrix sizes must be equal')
 else
-
-C = zeros(size(A));
-   
-for i=1:size(A,1)
-    for j=1:i
-        for k=j:i
-            C(i,j) = C(i,j) + A(i,k)*B(k,j);
+    C = zeros(size(A));
+    for i=1:size(A,1)
+        for j=1:i
+            for k=j:i
+                C(i,j) = C(i,j) + A(i,k)*B(k,j);
+            end
         end
     end
 end
-   
+
 end
